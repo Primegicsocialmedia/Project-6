83a2ea7cb3229b7f1b41b0ba9abcad95c31a19a3
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-13 16:56:09 +0200

    cards text busy

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Single from "../assets/single.png";
+import Double from "../assets/double.png";    
+import Triple from "../assets/triple.png";
 
 const Cards = () => {
   return (
-    <div>Cards</div>
-  )
-}
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img src={Single} alt="/" className="w-20 mx-auto mt-[-3rem] bg-white" />
+          <h2 className="text-2xl font-bold text-center py-8">Single User</h2>
+            <p className="text-center text-4xl font-bold">$149</p>
+            <div>
+                <p>500 GB Storage</p>
+                <p>1 Granted User</p>
+                <p>Send Up to 2 GB</p>
+            </div>
+            <button>Start Trial</button>
+        </div>
 
-export default Cards
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
