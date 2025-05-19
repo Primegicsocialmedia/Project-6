aada0575109e815b25e9041c2cfc01edd8dd5eba
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-19 13:09:43 +0200

    Footer complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
 
-    const [nav, setNav] = useState(false);
-
-    const handleNav = () => {
-        setNav(!nav);
-    };  
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white ">
@@ -20,22 +19,27 @@ const Navbar = () => {
         <li className="p-4">Contact</li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <AiOutlineMenu onClick={handleNav} size={20} /> : (<AiOutlineClose  size={20} />)}
-       
+        {nav ? (
+          <AiOutlineClose onClick={handleNav} size={20} />
+        ) : (
+          <AiOutlineMenu onClick={handleNav} size={20} />
+        )}
       </div>
-      <div
-        className={!nav ? `fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500` : `fixed left-[100%]`}
+      <ul
+        className={
+          nav
+            ? `fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500`
+            : `fixed left-[100%]`
+        }
       >
         <h1 className="w-full text-3xl font-bold text-primary m-4">Prime.</h1>
 
-        <ul className=" uppercase p-4">
-          <li className="p-4 border-b border-gray-600">Home</li>
-          <li className="p-4 border-b border-gray-600">Company</li>
-          <li className="p-4 border-b border-gray-600">Resourses</li>
-          <li className="p-4 border-b border-gray-600">About</li>
-          <li className="p-4 border-b border-gray-600">Contact</li>
-        </ul>
-      </div>
+        <li className="p-4 border-b border-gray-600">Home</li>
+        <li className="p-4 border-b border-gray-600">Company</li>
+        <li className="p-4 border-b border-gray-600">Resourses</li>
+        <li className="p-4 border-b border-gray-600">About</li>
+        <li className="p-4 border-b border-gray-600">Contact</li>
+      </ul>
     </div>
   );
 };
